1b74f6e06c74186bd2a983185ae5ceb8389edac8
Vitor Paulino <vitorpaulinog@gmail.com>
2024-08-25 00:15:13 -0300

    Fix EntityManager instatiation

--- src/main/java/dao/GenericDAO.java
+++ src/main/java/dao/GenericDAO.java
@@ -9,16 +9,29 @@ import jpautil.JPAUtil;
 
 public class GenericDAO<T> {
 	
-	private EntityManager entityManager = JPAUtil.getEntityManager();
+	protected EntityManager entityManager = JPAUtil.getEntityManager();
 	
 	public void salvar (T entidade)
 	{
+		
 		EntityTransaction transaction = entityManager.getTransaction();
 		transaction.begin();
 		
 		entityManager.persist(entidade);
+		
 		transaction.commit();
-		entityManager.close();
+//		entityManager.close();
+	}
+	public T mesclar (T entidade)
+	{
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		T retorno = entityManager.merge(entidade);
+		
+		transaction.commit();
+//		entityManager.close();
+		return retorno;
 	}
 	public T buscar (T entidade)
 	{
@@ -37,5 +50,27 @@ public class GenericDAO<T> {
 		transaction.commit();
 		return lista;
 		
+	}
+	public List<T> listarOrdenado (Class<T> entidade)
+	{
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		List<T> lista = entityManager.createQuery("from " + entidade.getName() + " order by id ASC").getResultList();
+		
+		transaction.commit();
+		return lista;
+		
+	}
+	
+	public void remover (T entidade)
+	{
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		entityManager.remove(entidade);
+		
+		transaction.commit();
+//		entityManager.close();
 	}
 }
